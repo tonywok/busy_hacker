b9847364e7f7a7dbdcf5c04639d70219733d0e8e
Tony Schneider <tonywok@gmail.com>
2010-12-03 18:55:12 -0500

    ditch rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BusyHacker::Application.config.secret_token = '676cef62790f888c0ce150d01cba2e29459bbc489456c641a7c8dcd6f02fe6cde0ae70338112466badbd53ba5023092a06a936cf9946eb62fad74bb5ecfa90ef'
