b9acd9042c5862f1f8769121d77d453dd5b422b8
Tony Schneider <tonywok@gmail.com>
2010-12-04 17:18:12 -0500

    mongo appears to be working

--- busy_hacker.rb
+++ busy_hacker.rb
@@ -1,5 +1,20 @@
 require 'uri'
 
+configure do
+  environment     = ENV['RACK_ENV'] || 'development'
+  COLLECTION_NAME = ENV['BUSY_HACKER_COLLECTION'] || "#{environment}_emails"
+
+  if connection_str = ENV['MONGOHQ_URL']
+    uri  = URI.parse(connection_str)
+    conn = Mongo::Connection.from_uri(connection_str)
+    DATABASE = conn.db(uri.path.gsub(/^\//, ''))
+    DATABASE.authenticate(uri.user, uri.password)
+  else
+    conn = Mongo::Connection.from_uri('mongodb://localhost')
+    DATABASE = conn.db("busyhacker_#{environment}")
+  end
+end
+
 get '/' do
   haml :index
 end
@@ -8,3 +23,11 @@ get '/style.css' do
   content_type 'text/css', :charset => 'utf-8'
   scss :style
 end
+
+post '/subscribe' do
+  email = params[:email]
+  coll = DATABASE.collection(COLLECTION_NAME)
+  doc = {"email" => email}
+  coll.insert(doc)
+end
+
--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
 require 'sinatra'
+require 'bundler'
+
+Bundler.require
+
 require './busy_hacker'
+
 run Sinatra::Application
