334ce276820bf4f6ebb9ee8c74affd624ed0dfd5
Tony Schneider <tonywok@gmail.com>
2010-12-05 14:36:05 -0500

    Add some basic mongo and app unit tests for sanity
    - removed some puts statements
    - minor style fix

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,6 @@ class Scraper
   end
 
   def self.scrape_hn
-
     doc = Nokogiri::HTML(open('http://news.ycombinator.com'))
 
     doc.xpath('//table')[2].children.each_slice(3).each do |data_block|
@@ -17,27 +16,16 @@ class Scraper
       if span
         hacker_id = span.attr(:id).gsub('score_', '').to_i
         score     = span.children.text.gsub(' points', '').to_i
+        article   = articles.find({hacker_id: hacker_id}).first
 
-        require 'ruby-debug'; Debugger.start; Debugger.settings[:autoeval] = 1; Debugger.settings[:autolist] = 1; debugger
-
-
-        puts "\n\n\n********************"
-        puts "Hacker id: #{hacker_id}"
-        puts "Score    : #{score}"
-
-        # if article
-        #   article.update_attributes(:score => score)
-        # else
+        if article
+          Scraper.articles.update({'_id' => article['_id']}, {'$set' => {'score' => score}})
+        else
           link    = data_block[0].css('a:first').last
           url     = link.attr(:href)
           title   = link.children.to_s
-
           articles.insert({url: url, title: title, score: score, hacker_id: hacker_id})
-
-          # Article.create(:score => score, :title => title, :url => url, :hacker_id => hacker_id)
-          puts "Url   : #{url}"
-          puts "Title : #{title}"
-        # end
+        end
       end
     end
   end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,7 +1,45 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Scraper do
-  it 'exists' do
-    fail
+  it 'has access to scraper collection' do
+    Scraper.articles.should be_a(Mongo::Collection)
+  end
+
+  context 'when an article is already in the db' do
+    let(:doc) { {score: 40,
+                 title: 'Google buys Busy Hacker for 1.3 Billion',
+                 url: 'www.tonyschneider.com',
+                 hacker_id: 3} }
+    before do
+      Scraper.articles.drop()
+      Scraper.articles.insert(doc)
+    end
+    after  { Scraper.articles.drop() }
+
+    it 'can find the document' do
+      article = Scraper.articles.find({hacker_id: 3}).first
+      article['score'].should == 40
+    end
+
+    it 'updates the document' do
+      article = Scraper.articles.find({hacker_id: 3}).first
+      Scraper.articles.update({"_id" => article['_id']}, {'$set' => {'score' => 30}})
+      article = Scraper.articles.find({hacker_id: 3}).first
+      article['score'].should == 30
+    end
+  end
+
+  context 'when an article is new' do
+    let(:doc) { {score: 40,
+                 title: 'Google buys Busy Hacker for 1.3 Billion',
+                 url: 'www.tonyschneider.com',
+                 hacker_id: 3} }
+    before { Scraper.articles.drop() }
+    after  { Scraper.articles.drop() }
+
+    it 'inserts the document' do
+      Scraper.articles.insert(doc)
+      Scraper.articles.count.should == 1
+    end
   end
 end
